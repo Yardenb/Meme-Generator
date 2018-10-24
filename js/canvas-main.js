b0a7159c891b2c6f5452079c36aa801c4499f557
thelastpako <pakokap1990@gmail.com>
2018-10-24 12:00:02 +0300

    text shadow

--- js/canvas-main.js
+++ js/canvas-main.js
@@ -2,27 +2,32 @@
 var canvas;
 var gCanvas;
 
-
+//first canvas choose by resolution
 function initCanvas() {
     let width = window.innerWidth;
     console.log(width);
     if (width > 750) {
         document.querySelector('.canvas-holder').innerHTML = `<canvas class="canvas-small" id="canvas" height="466" width="700">`;
+
     } else {
-        document.querySelector('.canvas-holder').innerHTML = `<canvas class="canvas-small" id="canvas" height="266" width="400" >`
+        document.querySelector('.canvas-holder').innerHTML = `<canvas class="canvas-small" id="canvas" height="266" width="400" >`;
+
     }
     canvas = document.getElementById('canvas');
     gCanvas = canvas.getContext("2d");
 }
+
+//clear the canvas
 function onClearCanvas() {
     gCanvas.clearRect(0, 0, canvas.width, canvas.height)
 }
 
+//save canvas
 function onSaveCanvas() {
     saveCanvas()
 }
 
-
+//draw the image
 function drawImage(elImg) {
     // gCanvas.drawImage(elImg, 0, 0);
     //if img is not ready yet, don't try to draw + try to draw in 50 ms
@@ -33,10 +38,14 @@ function drawImage(elImg) {
 
     gCanvas.drawImage(elImg, 0, 0, canvas.width, canvas.height)
 }
+
+//create the txt to canvas
 function createText(txt) {
     var elStroke = document.querySelector(`.stroke-check-${txt}`).checked
+    var elShadow = document.querySelector(`.shadow-check-${txt}`).checked
     var str = document.querySelector(`.${txt}`).value
     let font = gMeme[txt]
+    checkShadow(font.shadowColor,txt)    
     gCanvas.lineWidth = 1
     gCanvas.strokeStyle = font.stroke;
     gCanvas.textAlign = font.align;
@@ -47,13 +56,24 @@ function createText(txt) {
         gCanvas.strokeText(str, font.x, font.y);
     }
 }
+
+//draw canvas after checking prefernces
 function draw() {
     let el = getCurrImgEl();
     drawImage(el);
     createText('txtup')
-    createText('txtdown')
+    // createText('txtdown')
 }
 
+//check shadow text box and color
+function checkShadow(color,txt){
+    gCanvas.shadowColor = color;
+    var elShadow = document.querySelector(`.shadow-check-${txt}`).checked
+    gCanvas.shadowOffsetX = elShadow? 2:0
+    gCanvas.shadowOffsetY = elShadow? 2:0
+     elShadow = document.querySelector(`.shadow-blur-check-${txt}`).checked
+     gCanvas.shadowBlur = elShadow? 10:0
+}
 
 //change text color and draw new canvas
 function onChangeColor(txt, color) {
@@ -82,5 +102,11 @@ function onChangeAlign(txt, align) {
 //change text stroke color and draw to new canvas
 function onChangeStrokeColor(txt, color) {
     changeStrokeColor(txt, color)
+    draw()
+}
+
+//change shadow color and draw to new canvas if true
+function onChangeShadowColor(txt, color) {
+    changeShadowColor(txt, color)
     draw()
 }
